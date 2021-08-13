4a0cd8e25b30b5e28f1fda70ec137cfd268586df
DamianBusby-NDDB <damianbusby1947@gmail.com>
2021-08-13 18:23:20 -0500

    Work In Progress discord.js Bot

--- src/launchai-bot.js
+++ src/launchai-bot.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
 const PREFIX = "la!";
-const welcomeChannelID = 875859071121620992;
 
 bot.once('ready', () => {
     console.log(`${bot.user.username} has logged in.`);
@@ -13,12 +12,13 @@ bot.on('guildMemberAdd', (member) => {
     if (member.user.bot) return;
     const welcomeEmbed = new MessageEmbed()
         .setColor('#03038C')
-        .setTitle(`Congratulations Recruit @${member.user.tag}!`)
+        .setTitle('Congratulations Recruit!')
         .setThumbnail('https://cdn.discordapp.com/attachments/547250406070157320/875860415731609641/nuclear_wolves_server_icon_v3.jpg')
-        .setDescription('You have been officially recruited into the Nuclear Wolves Division!\nUpon being recruited all of your other servers will be instantly nuked by order of the Alpha Commander!')
-        .addField(' ', 'Head over to #laws for briefing before introducing yourself to the other members!')
-        .setImage('https://tenor.com/view/explosion-mushroom-cloud-atomic-bomb-bomb-boom-gif-4464831');
-    member.guild.publicUpdatesChannel.id(welcomeChannelID).send({ embeds: [welcomeEmbed] });
+        .setDescription(`Recruit ${member.user.tag}\n\nYou have been officially recruited into the Nuclear Wolves Division!\n\nUpon being recruited all of your other servers will be instantly nuked by order of the Alpha Commander!`)
+        .addField('\u200B', 'Head over to #laws for briefing before introducing yourself to the other members!')
+        .setImage('https://cdn.discordapp.com/attachments/547250406070157320/875875257616064572/nuke.gif');
+    member.guild.channels.cache.get('875859071121620992').send({ embeds: [welcomeEmbed] });
+    console.log("Member Joined!");
 })
 
 bot.on('messageCreate', (message) => {
